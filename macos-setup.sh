5388c58df020080dbd290fb56ca7d841f079ac86
Steve <steve@tech-otaku.com>
2021-02-24 19:13:06 +0000

    Update

--- macos-setup.sh
+++ macos-setup.sh
@@ -228,7 +228,6 @@ main () {	# See https://stackoverflow.com/questions/13588457/forward-function-de
 			move_directory_entry "D" "$SOURCE/Library/Application Scripts/com.barebones.bbedit" "/Users/steve/Library/Application Scripts/com.barebones.bbedit"
             ;;
 		books)	# Books.app
-        /Users/steve/Library/Containers/com.apple.BKAgentService
 			move_directory_entry "D" "$SOURCE/Library/Containers/com.apple.BKAgentService" "/Users/steve/Library/Containers/com.apple.BKAgentService"
 			;;
 		boxsync) 		# Box Sync.app
